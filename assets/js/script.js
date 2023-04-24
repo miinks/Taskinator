049a7d2654e642d2e6b86d111bf3bd479e470c26
Minks <jiyuniur@gmail.com>
2023-04-24 16:10:26 -0500

    tweaked listed items to properly display

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+     listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
+    
   };
 
   formEl.addEventListener("submit", createTaskHandler);
