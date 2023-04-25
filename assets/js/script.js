db74a765116ab1535a792b9d459536acf28150e3
Minks <jiyuniur@gmail.com>
2023-04-25 16:19:16 -0500

    added taskDataObj

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,28 @@ var createTaskHandler = function(event) {
 var taskNameInput = document.querySelector("input[name='task-name']").value;
 var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  createTaskEl(taskDataObj);
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-     listItemEl.appendChild(taskInfoEl);
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);
     
   };
+
+  var createTaskEl = function(taskDataObj) {
+
+  }
 
   formEl.addEventListener("submit", createTaskHandler);
