4b5d9ac87749b2d18be1ce7888f5050eb94ec330
Minks <jiyuniur@gmail.com>
2023-04-26 11:51:19 -0500

    fixed js issues regarding return statements and querySelectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,21 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
 
 var taskNameInput = document.querySelector("input[name='task-name']").value;
 var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+if (!taskNameInput === "" || !taskTypeInput === "") {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+formEl.reset();
+
+document.querySelector("input[name='task-name']").value = "";
+document.querySelector("select[name='task-type']").selectedIndex = 0;
 
 var taskDataObj = {
     name: taskNameInput,
@@ -21,23 +31,12 @@ var taskDataObj = {
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    
     listItemEl.appendChild(taskInfoEl);
+
+    console.dir(listItemEl);
 
     tasksToDoEl.appendChild(listItemEl);
     
   };
-
-  if (!taskNameInput || !taskTypeInput) {
-    alert("You need to fill out the task form!");
-    return false;
-  }
-
-  formEl.reset();
-
-
-  var createTaskEl = function(taskDataObj) {
-
-  }
 
   formEl.addEventListener("submit", createTaskHandler);
