bd86c4335a2ee8324f90b79062bca8968ec0786c
Minks <jiyuniur@gmail.com>
2023-04-28 18:17:07 -0500

    added createtaskActions

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,47 @@ var taskDataObj = {
   };
 
   createTaskEl(taskDataObj);
+  
+var createTaskActions = function(taskId) {
+var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+    
+    actionContainerEl.appendChild(editButtonEl);
+
+var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    return actionContainerEl;
+
+var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(statusSelectEl);
+
+var statusChoices = ["To Do", "In Progress", "Completed"];
+
+for (var i = 0; i < statusChoices.length; i++) {
+    var statusOptionEl = document.createElement("option");
+    statusOptionEl.textContent = statusChoices[i];
+    statusOptionEl.setAttribute("value", statusChoices[i]);
+
+    statusSelectEl.appendChild(statusOptionEl);
+}
+  
+  };
+
+
 
 };
 var createTaskEl = function(taskDataObj) {
