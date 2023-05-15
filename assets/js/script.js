c248ed590dcad0ce5619854853544f321d2ee921
Minks <jiyuniur@gmail.com>
2023-05-15 15:32:10 -0500

    new declarations

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelectorAll("#page-content");
+var formEl = document.querySelector("#task-form");
+
 
 
 var tasks = [];
